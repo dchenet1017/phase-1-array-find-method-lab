98aaf4066d1a2a411863c7b6833bb658773b85ac
Dimitri Chenet <dchenet1017@gmail.com>
2021-09-25 11:25:29 -0500

    commit tree 5e9342c89e81c97378a7b47857de50c657a8b5d1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+function superbowlWin(matches){
+    let results = matches.find(element => {
+        if(element.result === 'W' ){
+            return true
+        }
+
+    })
+    console.log(results)
+    if (results){
+        return results.year
+    } else{
+        return undefined 
+    }
+    
+}
